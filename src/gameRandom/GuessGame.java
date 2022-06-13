a64ba63e707957af862fe38f1ad98c7ff9e54451
POPOVVITPAV <POPOVVITPAV@yandex.ru>
2022-06-13 20:02:21 +0300

    finish game

--- src/gameRandom/GuessGame.java
+++ src/gameRandom/GuessGame.java
@@ -14,17 +14,45 @@ public class GuessGame {
         int numPl2;
         int numPl3;
 
+        boolean p1IsRight = false;
+        boolean p2IsRight = false;
+        boolean p3IsRight = false;
+
         int targetNum = (int) (Math.random() * 100);
         System.out.println("Computer num is 0...100");
         System.out.println(targetNum);
+        while (true) {
+            pl1.guess();
+            pl2.guess();
+            pl3.guess();
 
-        pl1.guess();
-        pl2.guess();
-        pl3.guess();
+            numPl1 = pl1.number;
+            System.out.println("Первый игрок думает, что это " + numPl1);
+            numPl2 = pl2.number;
+            System.out.println("Второй игрок думает, что это " + numPl2);
+            numPl3 = pl3.number;
+            System.out.println("Третий игрок думает, что это " + numPl3);
 
-        numPl1 = pl1.number;
-        numPl2 = pl2.number;
-        numPl3 = pl3.number;
+            if (numPl1 == targetNum) {
+                p1IsRight = true;
+            }
+            if (numPl2 == targetNum) {
+                p2IsRight = true;
+            }
+            if (numPl3 == targetNum) {
+                p3IsRight = true;
+            }
+            if (p1IsRight || p2IsRight || p3IsRight) {
+                System.out.println("У нас есть победитель:");
+                System.out.println("Превый игрок угадал? " + p1IsRight);
+                System.out.println("Второй игрок угадал? " + p2IsRight);
+                System.out.println("Третий игрок угадал? " + p3IsRight);
+                System.out.println("Конец");
+                break;
+            } else {
+                System.out.println("Игроки должны попробовать еще раз");
+            }
+        }
 
     }
 }
